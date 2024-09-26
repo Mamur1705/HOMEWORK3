9e0bf5cfa2fdd76b305eb63efffb1622fe3b7b66
Mamur <m.xushvaqtov@newuu.uz>
2024-09-26 17:27:40 +0500

    hw3

---
+++ main.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Mamur Xushvaqtov on 26/09/24.
+//
+
+#include "main.h"
---
+++ main.h
@@ -0,0 +1,8 @@
+//
+// Created by Mamur Xushvaqtov on 26/09/24.
+//
+
+#ifndef MAIN_H
+#define MAIN_H
+
+#endif //MAIN_H
